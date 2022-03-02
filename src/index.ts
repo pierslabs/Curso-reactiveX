ed42a270e542711eb01edd459baabaaadd21dc84
pierslabs <pedrolosasp@gmail.com>
2022-03-02 20:05:57 +0100

    Subject

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-import { Observable, Observer} from 'rxjs'
+import { Observable, Observer, Subject} from 'rxjs'
 
 const observer: Observer<any> = {
   next: value => console.log( 'siguiente [next]:', value),
@@ -7,35 +7,27 @@ const observer: Observer<any> = {
   complete: () => console.info('complete')
 }
 
-
-const intervalo$ = new Observable<number>( subs =>  {
-  // crear contador
-  let count = 0;
-
-  const interval = setInterval(()=> {
-    // cada segundo
-    count ++;
-    subs.next(count)
-
-  },1000);
-
-  return ()=>{
-    clearInterval(interval)
-    console.log('intervalo destruido')
-  }
-
-});
+const intervalo$ =  new Observable<number>( subs => {
 
 
-const subscription1 = intervalo$.subscribe( num => console.log( 'Num:', num))
-// const subscription2 = intervalo$.subscribe( num => console.log( 'Num:', num))
-const subscription3 = intervalo$.subscribe( num => console.log( 'Num:', num))
+  const intervalID = setInterval(
+      ()=> subs.next(Math.random()
+    )
+    ,3000)
+})
 
-setTimeout(()=>{
-  // se ejecuta pasado 3s
-  subscription1.unsubscribe()
-  subscription3.unsubscribe()
 
-  const subscription2 = intervalo$.subscribe( num => console.log( 'Num:', num))
+/*
+**propiedades**
+1- casteo multiple
+2-También es un observer
+3- Next erro y complete
+*/
 
-},3000)
+const subject$ = new Subject();
+intervalo$.subscribe(subject$)
+
+//las subscripciones son las mismas los humeros son iguales
+
+const subs1 = subject$.subscribe(rnd => console.log('subs1',rnd))
+const subs2 = subject$.subscribe(rnd => console.log('subs2',rnd))
